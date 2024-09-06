c521b2b9f420cdae9126df4f518e1f1bd9a738b7
LlyanaP <pollyanacostasousa@gmail.com>
2024-09-05 21:35:48 -0300

    Tela de cadastro de Livro

---
+++ pw3-pollyana-app-libri/src/components/forms/Select.jsx
@@ -0,0 +1,19 @@
+import styles from "./Select.module.css"
+
+function Select({type, name, text}){
+    return(
+        <div className={styles.form_control}>
+
+            <label htmlFor={name}>{text}</label>
+            <select name={name} id={name}>
+                <option>Selecione uma categoria</option>
+                <option>Ficção cientifica</option>
+                <option>Fantasia Heroica</option>
+                <option>Suspense</option>
+                <option>Terror</option>
+            </select>
+        </div>
+    )
+}
+
+export default Select
--- pw3-pollyana-app-libri/src/components/pages/CreateBooks.jsx
+++ pw3-pollyana-app-libri/src/components/pages/CreateBooks.jsx
@@ -1,10 +1,39 @@
 import React from "react";
 import style from "./CreateBooks.module.css";
-
+import Input from "../forms/Input";
+import Select from "../forms/Select";
+import Button from "../forms/Button";
 const CreateBooks = () =>{
     return(
         <section className={style.create_books_container}>
-            <h1>Create Book</h1>
+            <h1>Cadastro de livros</h1>
+            <Input
+                type="text"
+                name="txt_livro"
+                placeHolder="Digite o nome do seu livro aqui"
+                text="Título do livro"
+            />
+
+            <Input
+                type="text"
+                name="txt_autor"
+                placeHolder="Digite o nome do autor"
+                text="Nome do autor"
+            />
+
+            <Input
+                type="text"
+                name="txt_descrição_livro"
+                placeHolder="Digite a descrição do livro"
+                text="Descrição do livro"
+            />
+            <Select
+                name='Categoria'
+                text='Escolha uma categoria de livro'
+            />
+            <Button
+                rotulo='Cadastrar Livro'
+            />
         </section>
     )
 }
